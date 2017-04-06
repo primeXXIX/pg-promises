e67b4acf5b71124e06117c59720845f706fd6cd2
B.O.A.T <PRIME29@MODEL-PRIME.local>
2017-04-05 21:14:45 -0700

    adds exercises 8, 9 is unfinished

--- 08-values-and-promises.js
+++ 08-values-and-promises.js
@@ -2,7 +2,7 @@
 // promise handlers will wrap your returned values in promises allowing
 // additional chaining.
 //
-//   * Declare a function `attachTitle` which prepends `'DR. '` to its firstargument and returns the result.
+//   * Declare a function `attachTitle` which prepends `'DR. '` to its first argument and returns the result.
 //   * Create a fulfilled promise with a value of `'MANHATTAN'`.
 //   * Build a promise chain off the promise we just constructed that first calls`attachTitle` then calls `console.log`.
 //
@@ -10,6 +10,6 @@ function attachTitle(value) { //
   return 'DR. ' + value;      //prepends...to 'value'
 }
 
-Promise.resolve('MANHATTAN') //fulfilled promise with value of:
+Promise.resolve('MANHATTAN') //fulfilled promise with value of: 'MANHATTAN'
   .then(attachTitle)         //calls the attachTitle
   .then(console.log);        //calls console.log
